5480936e9531b43e7aad5df7abbc42a767cb6d73
Sergio Hernández <sedxher@gmail.com>
2024-02-05 09:08:56 -0600

    Update gwz_sales.sql
    se agregó el costo por día.
    se cambió de course14 a course16 por tener más información

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,7 +1,8 @@
 SELECT
   date_date,
-  ROUND(SUM(turnover), 2) AS turnover
+  ROUND(SUM(turnover), 2) AS turnover,
+  ROUND(SUM(purchase_cost), 2) AS purchase_cost
 FROM
-  `leafy-summer-412013.course14.gwz_sales`
+  `leafy-summer-412013.course16.gwz_sales`
 GROUP BY date_date
 ORDER BY date_date
